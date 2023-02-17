229759148103aea5ba99acce8ecdcdd312c90d5e
Paul Kruse <kruse.paul@epa.gov>
2023-02-17 12:40:20 -0500

    Initial commit.

---
+++ R/chemical-APIs.R
@@ -0,0 +1,28 @@
+#' Retrieve chemical information
+#'
+#' @param DTXSID The chemical identifier DTXSID
+#' @param API_key The user-specific API Key
+#' @return A data.frame containing chemical information for the chemical with
+#'   DTXSID matching the input parameter.
+
+
+get_chem_info <- function(DTXSID = NULL, API_key = NULL){
+  if (is.null(DTXSID))
+    stop('Please input a DTXSID!')
+  else if (is.null(API_key))
+    stop('Please input an API_key')
+
+  response <- httr::GET(url = paste0('https://api-ccte.epa.gov/chemical/property/search/by-dtxsid/', DTXSID),
+                        httr::add_headers(.headers = c(
+                          'Content-Type' =  'application/json',
+                          'x-api-key' = API_key)
+                          )
+                        )
+
+  if(response$status_code == 200){
+    return(jsonlite::fromJSON(content(response, as = 'text')))
+  } else {
+    print(response$status_code)
+  }
+ return()
+}
