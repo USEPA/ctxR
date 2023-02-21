a408425be939b7eea163c575a946b0d6b56d3939
Paul Kruse <kruse.paul@epa.gov>
2023-02-21 11:45:19 -0500

    Added function `get_chemical_mrv_by_dtxsid`.

--- R/chemical-APIs.R
+++ R/chemical-APIs.R
@@ -313,3 +313,43 @@ get_all_public_chemical_lists <- function(API_key = NULL){
   return()
 
 }
+
+
+#' Get mrv file by DTXSID
+#'
+#' @param DTXSID The chemical identifer DTXSID
+#' @param API_key The user-specific API key
+#'
+#' @return XML file format for representing a mrv file.
+#' @export
+
+
+get_chemical_mrv_by_dtxsid <- function(DTXSID = NULL,
+                                       API_key = NULL){
+  if (is.null(DTXSID))
+    stop('Please input list_name!')
+  else if (is.null(API_key))
+    stop('Please input an API_key!')
+
+  response <- httr::GET(url = paste0('https://api-ccte.epa.gov/chemical/file/mrv/search/by-dtxsid/', DTXSID),
+                        httr::add_headers(.headers = c(
+                          'Content-Type' =  'application/json',
+                          'x-api-key' = API_key)
+                        )
+  )
+
+  if(response$status_code == 200){
+    return(httr::content(response))
+    #return(jsonlite::fromJSON(httr::content(response, as = 'text')))
+  } else {
+    print(paste0('The request was unsuccessful, returning an error of ', response$status_code, '!'))
+  }
+  return()
+
+
+
+
+
+
+
+}
