7b9a4e35200773814fd4f0a1d36d87e2f147ea72
Paul Kruse <kruse.paul@epa.gov>
2023-04-24 12:15:25 -0400

    Added function `get_genetox_details`.

--- R/hazard-APIs.R
+++ R/hazard-APIs.R
@@ -190,3 +190,35 @@ get_genetox_summary <- function(DTXSID = NULL,
   return()
 
 }
+
+#' Get genetox details
+#'
+#' @param DTXSID The chemical identifier DTXSID
+#' @param API_key The user-specific API key
+#'
+#' @return A data.frame of genetox detail data related to the input DTXSID.
+#' @export
+
+
+get_genetox_details <- function(DTXSID = NULL,
+                                API_key = NULL){
+  if (is.null(DTXSID))
+    stop('Please input a DTXSID!')
+  else if (is.null(API_key))
+    stop('Please input an API_key')
+
+  response <- httr::GET(url = paste0('https://api-ccte-stg.epa.gov/hazard/genetox/details/search/by-dtxsid/', DTXSID),
+                        httr::add_headers(.headers = c(
+                          'Content-Type' =  'application/json',
+                          'x-api-key' = API_key)
+                        )
+  )
+
+  if(response$status_code == 200){
+    return(jsonlite::fromJSON(httr::content(response, as = 'text')))
+  } else {
+    print(paste0('The request was unsuccessful, returning an error of ', response$status_code, '!'))
+  }
+  return()
+
+}
