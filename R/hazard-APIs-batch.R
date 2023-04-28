8a54680bb413fdb9981036c78623a5351c51139f
Paul Kruse <kruse.paul@epa.gov>
2023-04-28 16:23:58 -0400

    Added function `get_cancer_hazard_batch`.

--- R/hazard-APIs-batch.R
+++ R/hazard-APIs-batch.R
@@ -176,3 +176,49 @@ get_skin_eye_hazard_batch <- function(DTXSID = NULL,
     stop('Please input a list of DTXSIDs!')
   }
 }
+
+
+#' Get cancer hazard batch
+#'
+#' @param DTXSID The chemical identifier DTXSIDs
+#' @param API_key The user-specific API key.
+#' @param rate_limit Number of seconds to wait between requests
+#'
+#' @return A named list of data.frames, each containing cancer hazard and
+#'   related data for each input DTXSID.
+#' @export
+
+
+get_cancer_hazard_batch <- function(DTXSID = NULL,
+                                    API_key = NULL,
+                                    rate_limit = 0L){
+  if (is.null(API_key) || !is.character(API_key)){
+    stop('Please input a character string containing a valid API key!')
+  }
+  if (!is.numeric(rate_limit) | (rate_limit < 0)){
+    rate_limit <- 0L
+  }
+  if (!is.null(DTXSID)){
+    DTXSID <- unique(DTXSID)
+    results <- lapply(DTXSID, function(t){
+      Sys.sleep(rate_limit)
+      attempt <- tryCatch(
+        {
+          get_cancer_hazard_by_dtxsid(DTXSID = t,
+                                      API_key = API_key)
+        },
+        error = function(cond){
+          message(t)
+          message(cond$message)
+          return(NA)
+        }
+      )
+      return(attempt)
+    }
+    )
+    names(results) <- DTXSID
+    return(results)
+  } else {
+    stop('Please input a list of DTXSIDs!')
+  }
+}
