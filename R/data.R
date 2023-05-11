5d5c566ea5eabc073051a511ad0cc392846e841a
Paul Kruse <kruse.paul@epa.gov>
2023-05-11 15:57:00 -0400

    Initial commit.

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Chemical API Server url
+#'
+#' A section of url used in Chemical API Endpoints
+'chemical_api_server'
+
+#' Hazard API Server url
+#'
+#' A section of url used in Hazard API Endpoints
+'hazard_api_server'
+
+#' Bioactivity API Server url
+#'
+#' A section of url used in Bioactivity API Endpoints
+'bioactivity_api_server'
+
+utils::globalVariables(c('chemical_api_server', 'hazard_api_server', 'bioactivity_api_server'))
