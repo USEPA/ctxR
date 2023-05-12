e9c25fa78b05e452d138cb6a03b0d3a0a02c2a72
Paul Kruse <kruse.paul@epa.gov>
2023-05-12 13:20:47 -0400

    Changed API key in test.

--- tests/testthat/test-bioactivity-APIs-batch.R
+++ tests/testthat/test-bioactivity-APIs-batch.R
@@ -16,7 +16,7 @@ test_that('Rate limit warnings', {
 })
 
 test_that('Return data types', {
-  expect_type(get_bioactivity_details_batch(DTXSID = c('DTXSID8031865'), API_key = 'test_key'), 'list')
+  expect_type(get_bioactivity_details_batch(DTXSID = c('DTXSID8031865'), API_key = ''), 'list')
   expect_type(get_bioactivity_details_batch(DTXSID = c('DTXSID8031865'), API_key = Sys.getenv('CCTE_API_KEY')), 'list')
   expect_type(get_bioactivity_details_batch(AEID = c(42), API_key = 'test_key'), 'list')
 })
