3c63ba4e1d9eba718969c8655a08f471f600bc03
Paul Kruse <kruse.paul@epa.gov>
2023-06-21 11:59:12 -0400

    Added `get_hazard_by_dtxsid_batch_2`, `get_human_hazard_by_dtxsid_batch_2`, `get_ecotox_hazard_by_dtxsid_batch_2`, and `create_hazard_data.table` functions.

--- R/hazard-APIs-batch.R
+++ R/hazard-APIs-batch.R
@@ -52,6 +52,98 @@ get_hazard_by_dtxsid_batch <- function(DTXSID = NULL,
   }
 }
 
+get_hazard_by_dtxsid_batch_2 <- function(DTXSID = NULL,
+                                         API_key = NULL,
+                                         rate_limit = 0L,
+                                         Server = hazard_api_server){
+  if (is.null(API_key) || !is.character(API_key)){
+    if (has_ccte_key()) {
+      API_key <- ccte_key()
+      message('Using stored API key!')
+    } else {
+      stop('Please input a character string containing a valid API key!')
+    }
+  }
+  if (!is.numeric(rate_limit) | (rate_limit < 0)){
+    warning('Setting rate limit to 0 seconds between requests!')
+    rate_limit <- 0L
+  }
+  if (!is.null(DTXSID)){
+    if (!is.character(DTXSID) & !all(sapply(DTXSID, is.character))){
+      stop('Please input a character list for DTXSID!')
+    }
+    DTXSID <- unique(DTXSID)
+    num_dtxsid <- length(DTXSID)
+    indices <- generate_ranges(num_dtxsid)
+
+    dt <- create_hazard_data.table()
+
+    for (i in seq_along(indices)){
+
+      print(paste('The current index is i =', i, 'out of', length(indices)))
+
+      response <- httr::POST(url = paste0(Server, '/search/by-dtxsid/'),
+                             httr::add_headers(.headers = c(
+                               'Accept' = 'application/json',
+                               'Content-Type' = 'application/json',
+                               'x-api-key' = API_key
+                             )),
+                             body = jsonlite::toJSON(DTXSID[indices[[i]]], auto_unbox = ifelse(length(DTXSID[indices[[i]]]) > 1, 'T', 'F')))
+
+      print(paste('The response code is', response$status_code, 'for index i =', i))
+
+
+      if (response$status_code == 200){
+        dt <- suppressWarnings(data.table::rbindlist(list(dt,
+                                                          data.table::data.table(jsonlite::fromJSON(httr::content(response,
+                                                                                                                  as = 'text')))),
+                                                     fill = TRUE))
+      }
+      Sys.sleep(rate_limit)
+    }
+
+    return(dt)
+  } else {
+    stop('Please input a list of DTXSIDs!')
+  }
+}
+
+create_hazard_data.table <- function(){
+  dt <- data.table::data.table(id = integer(),
+                               source = character(),
+                               year = character(),
+                               dtxsid = character(),
+                               exposureRoute = character(),
+                               toxvalNumeric = numeric(),
+                               toxvalNumericQualifier = character(),
+                               toxvalUnits = character(),
+                               studyType = character(),
+                               studyDurationClass = character(),
+                               studyDuractionValue = numeric(),
+                               studyDurationUnits = character(),
+                               strain = character(),
+                               sex = character(),
+                               population = character(),
+                               exposureMethod = character(),
+                               exposureForm = character(),
+                               media = character(),
+                               lifestage = character(),
+                               generation = character(),
+                               criticalEffect = character(),
+                               detailText = character(),
+                               supercategory = character(),
+                               speciesCommon = character(),
+                               humanEcoNt = character(),
+                               priorityId = integer(),
+                               subsource = character(),
+                               sourceUrl = character(),
+                               subsourceUrl = character(),
+                               riskAssessmentClass = character(),
+                               toxvalType = character(),
+                               toxvalSubtype = character())
+  return(dt)
+}
+
 #' Get human hazard data by DTXSID batch
 #'
 #' @param DTXSID A list of chemical identifier DTXSIDs.
@@ -105,6 +197,63 @@ get_human_hazard_by_dtxsid_batch <- function(DTXSID = NULL,
   }
 }
 
+get_human_hazard_by_dtxsid_batch_2 <- function(DTXSID = NULL,
+                                         API_key = NULL,
+                                         rate_limit = 0L,
+                                         Server = hazard_api_server){
+  if (is.null(API_key) || !is.character(API_key)){
+    if (has_ccte_key()) {
+      API_key <- ccte_key()
+      message('Using stored API key!')
+    } else {
+      stop('Please input a character string containing a valid API key!')
+    }
+  }
+  if (!is.numeric(rate_limit) | (rate_limit < 0)){
+    warning('Setting rate limit to 0 seconds between requests!')
+    rate_limit <- 0L
+  }
+  if (!is.null(DTXSID)){
+    if (!is.character(DTXSID) & !all(sapply(DTXSID, is.character))){
+      stop('Please input a character list for DTXSID!')
+    }
+    DTXSID <- unique(DTXSID)
+    num_dtxsid <- length(DTXSID)
+    indices <- generate_ranges(num_dtxsid)
+
+    dt <- create_hazard_data.table()
+
+    for (i in seq_along(indices)){
+
+      print(paste('The current index is i =', i, 'out of', length(indices)))
+
+      response <- httr::POST(url = paste0(Server, '/human/search/by-dtxsid/'),
+                             httr::add_headers(.headers = c(
+                               'Accept' = 'application/json',
+                               'Content-Type' = 'application/json',
+                               'x-api-key' = API_key
+                             )),
+                             body = jsonlite::toJSON(DTXSID[indices[[i]]], auto_unbox = ifelse(length(DTXSID[indices[[i]]]) > 1, 'T', 'F')))
+
+      print(paste('The response code is', response$status_code, 'for index i =', i))
+
+
+      if (response$status_code == 200){
+        dt <- suppressWarnings(data.table::rbindlist(list(dt,
+                                                          data.table::data.table(jsonlite::fromJSON(httr::content(response,
+                                                                                                                  as = 'text')))),
+                                                     fill = TRUE))
+      }
+      Sys.sleep(rate_limit)
+    }
+
+    return(dt)
+  } else {
+    stop('Please input a list of DTXSIDs!')
+  }
+}
+
+
 #' Get ecotox hazard data by DTXSID batch
 #'
 #' @param DTXSID A list of chemical identifier DTXSIDs.
@@ -153,6 +302,62 @@ get_ecotox_hazard_by_dtxsid_batch <- function(DTXSID = NULL,
     )
     names(results) <- DTXSID
     return(results)
+  } else {
+    stop('Please input a list of DTXSIDs!')
+  }
+}
+
+get_ecotox_hazard_by_dtxsid_batch_2 <- function(DTXSID = NULL,
+                                         API_key = NULL,
+                                         rate_limit = 0L,
+                                         Server = hazard_api_server){
+  if (is.null(API_key) || !is.character(API_key)){
+    if (has_ccte_key()) {
+      API_key <- ccte_key()
+      message('Using stored API key!')
+    } else {
+      stop('Please input a character string containing a valid API key!')
+    }
+  }
+  if (!is.numeric(rate_limit) | (rate_limit < 0)){
+    warning('Setting rate limit to 0 seconds between requests!')
+    rate_limit <- 0L
+  }
+  if (!is.null(DTXSID)){
+    if (!is.character(DTXSID) & !all(sapply(DTXSID, is.character))){
+      stop('Please input a character list for DTXSID!')
+    }
+    DTXSID <- unique(DTXSID)
+    num_dtxsid <- length(DTXSID)
+    indices <- generate_ranges(num_dtxsid)
+
+    dt <- create_hazard_data.table()
+
+    for (i in seq_along(indices)){
+
+      print(paste('The current index is i =', i, 'out of', length(indices)))
+
+      response <- httr::POST(url = paste0(Server, '/eco/search/by-dtxsid/'),
+                             httr::add_headers(.headers = c(
+                               'Accept' = 'application/json',
+                               'Content-Type' = 'application/json',
+                               'x-api-key' = API_key
+                             )),
+                             body = jsonlite::toJSON(DTXSID[indices[[i]]], auto_unbox = ifelse(length(DTXSID[indices[[i]]]) > 1, 'T', 'F')))
+
+      print(paste('The response code is', response$status_code, 'for index i =', i))
+
+
+      if (response$status_code == 200){
+        dt <- suppressWarnings(data.table::rbindlist(list(dt,
+                                                          data.table::data.table(jsonlite::fromJSON(httr::content(response,
+                                                                                                                  as = 'text')))),
+                                                     fill = TRUE))
+      }
+      Sys.sleep(rate_limit)
+    }
+
+    return(dt)
   } else {
     stop('Please input a list of DTXSIDs!')
   }
