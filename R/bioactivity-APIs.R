a8d1a793ed32ac54a00b388ed562287620299228
Paul Kruse <kruse.paul@epa.gov>
2024-01-03 10:38:03 -0500

    Added comments for expanding function get_bioactivity_details(). Added get_bioactivity_summary() function.

--- R/bioactivity-APIs.R
+++ R/bioactivity-APIs.R
@@ -13,10 +13,10 @@ get_bioactivity_details <- function(DTXSID = NULL,
                                  AEID = NULL,
                                  API_key = NULL,
                                  Server = bioactivity_api_server){
-  if (is.null(DTXSID) & is.null(AEID))
-    stop('Please input a DTXSID or AEID!')
-  else if (!is.null(DTXSID) & !is.null(AEID))
-    stop('Please input either a DTXSID or AEID, but not both!')
+  if (is.null(DTXSID) & is.null(AEID))# if (all(sapply(list(DTXSID, AEID, SPID, m4ID), is.null)))
+    stop('Please input a DTXSID or AEID!') # " or SPID or m4ID"
+  else if (!is.null(DTXSID) & !is.null(AEID)) # else if (length(which(!sapply(list(DTXSID, AEID, SPID, m4ID), is.null))) > 1)
+    stop('Please input either a DTXSID or AEID, but not both!') #  or SPID or m4ID, but not multiple!
   else if (is.null(API_key)){
     if (has_ccte_key()) {
       API_key <- ccte_key()
@@ -25,6 +25,9 @@ get_bioactivity_details <- function(DTXSID = NULL,
       stop('Please input an API_key!')
     }
   }
+
+  # data_index <- which(!sapply(list(DTXSID, AEID, SPID, m4ID), is.null))
+  # data_endpoint <- paste0('by-', c('dtxsid', 'aeid', 'spid', 'm4id')[data_index])
 
   if (!is.null(DTXSID)){
     response <- httr::GET(url = paste0(Server, '/search/by-dtxsid/', DTXSID),
@@ -49,4 +52,36 @@ get_bioactivity_details <- function(DTXSID = NULL,
     print(paste0('The request was unsuccessful, returning an error of ', response$status_code, '!'))
   }
   return()
+}
+
+get_bioactivity_summary <- function(AEID = NULL,
+                                    API_key = NULL,
+                                    Server = bioactivity_api_server){
+  print("This is broken currently!")
+  return()
+  if (is.null(AEID))
+    stop('Please input an AEID!')
+  else if (is.null(API_key)){
+    if (has_ccte_key()){
+      API_key <- ccte_key()
+      message('Using stored API key!')
+    } else {
+      stop('Please input an API_key!')
+    }
+  }
+
+  response <- httr::GET(url = paste0(Server, '/data/summary/search/by-aeid/', AEID),
+                        httr::add_headers(.headers = c(
+                          'Content-Type' =  'application/json',
+                          'x-api-key' = API_key)
+                        )
+  )
+
+  if(response$status_code == 200){
+    return(jsonlite::fromJSON(httr::content(response, as = 'text')))
+  } else {
+    print(paste0('The request was unsuccessful, returning an error of ', response$status_code, '!'))
+  }
+  return()
+
 }
