db7739d33de325a32d0d6e33d2e19c27e3d112ea
Paul Kruse <kruse.paul@epa.gov>
2024-01-31 12:30:30 -0500

    Added in Server parameter and associated function contents, created batch version of get_annotation_by_aeid function.

--- R/bioactivity-APIs-batch.R
+++ R/bioactivity-APIs-batch.R
@@ -13,6 +13,7 @@
 get_bioactivity_details_batch <- function(DTXSID = NULL,
                                           AEID = NULL,
                                           API_key = NULL,
+                                          Server = NULL,
                                           rate_limit = 0L){
   if (is.null(API_key) || !is.character(API_key)){
     if (has_ccte_key()) {
@@ -26,6 +27,11 @@ get_bioactivity_details_batch <- function(DTXSID = NULL,
     warning('Setting rate limit to 0 seconds between requests!')
     rate_limit <- 0L
   }
+
+  if (is.null(Server)){
+    Server <- bioactivity_api_server
+  }
+
   if (!is.null(DTXSID)){
     if (!is.character(DTXSID) & !all(sapply(DTXSID, is.character))){
       stop('Please input a character list for DTXSID!')
@@ -37,7 +43,8 @@ get_bioactivity_details_batch <- function(DTXSID = NULL,
       attempt <- tryCatch(
         {
           get_bioactivity_details(DTXSID = d,
-                                  API_key = API_key)
+                                  API_key = API_key,
+                                  Server = Server)
         },
         error = function(cond){
           message(d)
@@ -133,4 +140,64 @@ get_bioactivity_summary_batch <- function(AEID = NULL,
   } else {
     stop('Please input a list of AEIDs!')
   }
+}
+
+
+#' Retrieve annotations for AEID batch
+#'
+#' @param AEID A list of AEID identifiers
+#' @param API_key The user-specific API key
+#' @param Server The root address for the API endpoint
+#' @param rate_limit Number of seconds to wait between each request
+#'
+#' @return A named list of data.frames containing annotation information for the
+#'   assays with AEID matching the input parameter.
+
+
+get_annotation_by_aeid_batch <- function(AEID = NULL,
+                                         API_key = NULL,
+                                         Server = NULL,
+                                         rate_limit = 0L){
+  if (is.null(API_key) || !is.character(API_key)){
+    if (has_ccte_key()) {
+      API_key <- ccte_key()
+      message('Using stored API key!')
+    } else {
+      stop('Please input a character string containing a valid API key!')
+    }
+  }
+  if (!is.numeric(rate_limit) | (rate_limit < 0)){
+    warning('Setting rate limit to 0 seconds between requests!')
+    rate_limit <- 0L
+  }
+  if (is.null(Server)){
+    Server <- bioactivity_api_server
+  }
+
+  if (!is.null(AEID)){
+    AEID <- unique(AEID)
+    print('Using AEID!')
+    results <- lapply(AEID, function(a){
+      Sys.sleep(rate_limit)
+      attempt <- tryCatch(
+        {
+          get_annotation_by_aeid(AEID = a,
+                                 API_key = API_key,
+                                 Server = Server)
+        },
+        error = function(cond){
+          message(a)
+          message(cond$message)
+          return(NA)
+        }
+      )
+      return(attempt)
+    }
+    )
+    names(results) <- AEID
+    return(results)
+  } else {
+    stop('Please input a list of AEIDs!')
+  }
+
 }
