eea5334689fc7278ae11fcab3b77be07c2adb04a
Jason Brown <brown.jason@epa.gov>
2024-02-09 15:44:05 -0500

    Revert "Revert "example of mocking api data""
    This reverts commit 321e49a820505d215cfd6cc4e8a5e788d9fbc9c7.

--- tests/testthat/test-bioactivity-APIs.R
+++ tests/testthat/test-bioactivity-APIs.R
@@ -1,17 +1,12 @@
+with_mock_dir("bioactivity", {
 test_that("Catch missing API", {
   # Run register_ccdr(key = 'YOUR KEY', write = TRUE) prior to running tests
-  expect_message(get_bioactivity_details(DTXSID = 'DTXSID7020182'), 'Using stored API key!')
-})
-
-test_that("DTXSID/AEID errors", {
+  # expect_message(get_bioactivity_details(DTXSID = 'DTXSID7020182'), 'Using stored API key!')
   expect_error(get_bioactivity_details(API_key = 'test_key'), 'Please input a DTXSID, AEID, SPID, or m4id!')
   expect_error(get_bioactivity_details(DTXSID = 'DTXSID7020182', AEID = 1234, API_key = 'test_key'), 'Please input a value for only one of DTXSID, AEID, SPID, or m4id, but not multiple!')
-})
-
-test_that('Return data type', {
   expect_type(get_bioactivity_details(DTXSID = 'DTXSID7020182', API_key = Sys.getenv('CCTE_API_KEY')), 'list')
   expect_type(get_bioactivity_details(DTXSID = '', API_key = Sys.getenv('CCTE_API_KEY')), 'NULL')
   #expect_type(get_bioactivity_details(DTXSID = 'DTXSID7020182', API_key = ''), 'NULL')
-  expect_type(get_bioactivity_details(AEID = 42), 'list')
+  expect_type(get_bioactivity_details(AEID = 42, API_key = Sys.getenv('CCTE_API_KEY')), 'list')
   #expect_type(get_bioactivity_details(AEID = 42, API_key = ''), 'NULL')
-})
+})})
