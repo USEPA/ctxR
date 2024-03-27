1e10cdb83892a40c5586fd85d3ed34cba5f75ef0
Paul Kruse <kruse.paul@epa.gov>
2024-03-27 12:20:37 -0400

    Added functions to load API key into package environment when attached.

--- R/attach.R
+++ R/attach.R
@@ -19,6 +19,7 @@
     class = 'packageStartupMessage'
   )
 
+  .getKeyIntoPkgEnv(silent = FALSE)
   bootstrap_ccdr()
 
 }
@@ -34,4 +35,40 @@ bootstrap_ccdr <- function() {
   ),
   'display_api_key' = FALSE
   )
+}
+
+.pkgenv <- new.env(parent = emptyenv())
+
+.defaultFile <- function() {
+  if (getRversion() >= "4.0.0") {
+    ccdRdir <- tools::R_user_dir("ccdR")
+    if (dir.exists(ccdRdir)) {
+      fname <- file.path(ccdRdir, "api.dcf")
+      if (file.exists(fname)) {
+        return(fname)
+      }
+    }
+  }
+  return("")
+}
+
+.getKeyIntoPkgEnv <- function(silent = TRUE) {
+  .pkgenv[["api"]] <- ""
+  fname <- .defaultFile()
+  if (fname != "") {
+    res <- read.dcf(fname)
+    if (!is.na(match("key", colnames(res)))) {
+      .pkgenv[["api"]] <- res[[1, "key"]]
+      Sys.setenv('CCTE_API_KEY' = res[[1, "key"]])
+      if (!silent) packageStartupMessage("Setting API key from config file.")
+    } else {
+      if (!silent) packageStartupMessage("API key file found but no api entry.")
+    }
+  } else if ((ev <- Sys.getenv("CCTE_API_KEY")) != "") {
+    .pkgenv[["api"]] <- ev
+    if (!silent) packageStartupMessage("Setting API key from environment variable.")
+  } else {
+    if (!silent) packageStartupMessage(paste("No config file or environment variable",
+                                             "found: API access unlikely."))
+  }
 }
