58df5b0fcc5b131846f0e3f92841814bb93400aa
Paul Kruse <kruse.paul@epa.gov>
2024-04-04 16:16:57 -0400

    Corrected 'ccdr' to 'ccdR' and updated Roxygen to reflect that the API key is no longer stored in the .Renviron file.

--- R/register_ccte.R
+++ R/register_ccte.R
@@ -7,27 +7,27 @@
 #' \url{https://api-ccte.epa.gov/docs/}. This documentation shows you how to
 #' obtain an API key to allow access to the CCTE APIs.
 #'
-#' To tell ccdr about your API key, use [register_ccte()], e.g.
+#' To tell ccdR about your API key, use [register_ccte()], e.g.
 #' `register_ccte(key = 'grbwigbwoginrowgbwibgdibdvinrginiwgo')` (that's a fake
 #' key). This will set your API key for the current session, but if you restart
 #' R, you'll need to do it again. You can set it permanently by setting `write =
-#' TRUE`m see the examples. If you set it permanently it will be stored in your
-#' .Renviron file, and that will be accessed by ccdr persistently across
+#' TRUE`m see the examples. If you set it permanently it will be stored in a
+#' local file, and that will be accessed by ccdR persistently across
 #' sessions.
 #'
 #' Users should be aware that the API key, a string of garbled
 #' characters/numbers/symbols, is a PRIVATE key - it uniquely identifies and
 #' authenticates you to CCTE's services. If anyone gets your API key, they can
 #' use it to masquerade as you to CCTE. To mitigate against users inadvertently
-#' sharing their keys, by default ccdr never displays a user's key in messages
+#' sharing their keys, by default ccdR never displays a user's key in messages
 #' displayed to the console.
 #'
-#' Users should be aware that ccdr has no mechanism with which to safeguard the
+#' Users should be aware that ccdR has no mechanism with which to safeguard the
 #' private key once registered with R. That is to say, once you register your
-#' API key, any R function will have access to it. As a consequence, ccdr will
+#' API key, any R function will have access to it. As a consequence, ccdR will
 #' not know if another function, potentially from a compromised package,
 #' accesses the key and uploads it to a third party. For this reason, when using
-#' ccdr we recommend a heightened sense of security and self-awareness: only use
+#' ccdR we recommend a heightened sense of security and self-awareness: only use
 #' trusted packages, do not save the API keys in script files, etc.
 #'
 #' @param key an API key
@@ -81,7 +81,7 @@ showing_key <- function() {
 
 ccdr_show_api_key <- function() {
   set_ccdr_option('display_api_key' = TRUE)
-  cli::cli_alert_warning('ccdr will now display PRIVATE API keys in the console.')
+  cli::cli_alert_warning('ccdR will now display PRIVATE API keys in the console.')
   invisible()
 }
 
@@ -95,7 +95,7 @@ ccdr_show_api_key <- function() {
 
 ccdr_hide_api_key <- function() {
   set_ccdr_option('display_api_key' = FALSE)
-  cli::cli_alert_info('ccdr will now suppress private API keys in the console.')
+  cli::cli_alert_info('ccdR will now suppress private API keys in the console.')
   invisible()
 }
 
@@ -111,7 +111,7 @@ ccdr_hide_api_key <- function() {
 
 register_ccdr <- function(key, write = FALSE) {
 
-  # allow register_ccdr to work when ccdr not loaded
+  # allow register_ccdr to work when ccdR not loaded
   #
   # FILL in details
   #
