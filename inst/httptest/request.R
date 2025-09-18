6a6390dc660a071a7a3342dd075634963d598dc0
Kruse <krusepm@nih.gov>
2025-09-18 14:36:48 -0400

    Fixed one substitution causing issues and added a second substitution.

--- inst/httptest/request.R
+++ inst/httptest/request.R
@@ -1,10 +1,11 @@
 function (request) {
   request |>
     gsub_request("https://api-ccte.epa.gov/", "", fixed=TRUE) |>
-    gsub_request("https://comptox.epa.gov/ctx-api/", "ctx") |>
+    gsub_request("https://comptox.epa.gov/ctx-api/", "ctx/", fixed = TRUE) |>
     gsub_request("exposure-batch", "exp-batch", fixed = TRUE) |>
     gsub_request("functional-use", "fun-use", fixed = TRUE) |>
     gsub_request("exposure/seem/demographic", "expo-demo", fixed = TRUE) |>
     gsub_request("exposure/seem/general", "expo-gen", fixed = TRUE) |>
+    gsub_request("exposure/ccd", "ccd", fixed = TRUE) |>
     gsub_request("api-ccte-stg.epa.gov", "stg", fixed = TRUE)
   }
