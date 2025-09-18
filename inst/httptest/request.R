d844c9c57572d8aa6f97e3fa412f93c683ee2a7d
Kruse <krusepm@nih.gov>
2025-09-18 12:55:02 -0400

    Added new root path abbreviation.

--- inst/httptest/request.R
+++ inst/httptest/request.R
@@ -1,9 +1,10 @@
 function (request) {
   request |>
     gsub_request("https://api-ccte.epa.gov/", "", fixed=TRUE) |>
+    gsub_request("https://comptox.epa.gov/ctx-api/", "ctx") |>
     gsub_request("exposure-batch", "exp-batch", fixed = TRUE) |>
     gsub_request("functional-use", "fun-use", fixed = TRUE) |>
     gsub_request("exposure/seem/demographic", "expo-demo", fixed = TRUE) |>
     gsub_request("exposure/seem/general", "expo-gen", fixed = TRUE) |>
     gsub_request("api-ccte-stg.epa.gov", "stg", fixed = TRUE)
-}
+  }
